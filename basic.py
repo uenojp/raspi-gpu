eaac9daba560d45a7e2211ab81c01732ff3b54f7
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2023-08-05 23:03:55 +0900

    Add basic.py

--- basic.py
+++ basic.py
@@ -1,4 +1,21 @@
 #!/bin/env python3
 
-print('hello')
+from videocore6.assembler import qpu
+from videocore6.driver import Driver
 
+
+@qpu
+def kernel(asm):
+    nop()
+    exit()
+
+
+def main():
+    with Driver() as drv:
+        kernel_code = drv.program(kernel)
+        nqpus = 1
+        drv.execute(kernel_code, thread=nqpus)
+
+
+if __name__ == '__main__':
+    main()
